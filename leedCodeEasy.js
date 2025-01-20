3def1dfd42b7615e6e14bd63187c4cd4e3e31163
Rahul <rahulna1996@gmail.com>
2025-01-20 22:08:32 +0530

    Problem 2 solved

---
+++ leedCodeEasy.js
@@ -0,0 +1,43 @@
+// Problem 1 : Two Sum
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+var twoSum = function (nums, taget) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === taget) {
+        return [i, j];
+      }
+    }
+  }
+};
+console.log(twoSum([2, 7, 11, 15], 9)); // time complexity O(n^2)
+
+// Problem 2 : Remove Element
+/*
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+*/
+
+var removeElement = function (nums, val) {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+};
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // time complexity O(n)
