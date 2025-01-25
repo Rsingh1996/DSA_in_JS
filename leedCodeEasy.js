e25802b1854c7159c75035fd1317ba412405ca4d
Rahul <rahulna1996@gmail.com>
2025-01-26 00:43:29 +0530

    Problem 4 Plus One solved

--- leedCodeEasy.js
+++ leedCodeEasy.js
@@ -66,3 +66,30 @@ var lengthOfLastWord = function (s) {
   return length;
 };
 console.log(lengthOfLastWord("   fly me   to   the moon  ")); // time complexity O(n)
+
+// Problem 4 : Plus One
+
+/*
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+Increment the large integer by one and return the resulting array of digits.
+Example 
+Input: digits = [9,9]
+Output: [1,0,0]
+Explanation: The array represents the integer 99.
+Incrementing by one gives 99 + 1 = 100.
+Thus, the result should be [1,0,0].
+*/
+
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1);
+  return digits;
+};
+console.log(plusOne([9, 9])); // time complexity O(n)
