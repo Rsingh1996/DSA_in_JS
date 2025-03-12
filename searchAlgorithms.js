0626c936d15da48424132ecc2939d3861687b465
Rahul Singh <81622488+Rsingh1996@users.noreply.github.com>
2025-03-12 23:43:11 +0530

    recursive binary search

--- searchAlgorithms.js
+++ searchAlgorithms.js
@@ -17,7 +17,7 @@ console.log(linearSearch([-5, 2, 10, 4, 6], 6));  // Output: 4   // Big-O = O(n)
 console.log(linearSearch([-5, 2, 10, 4, 6], 1));  // Output: -1  // Big-O = O(n)
 
 
-//Problem 1: Gievn an sorted array of 'n' elements and a target element 't', find the index of 't' in the array. Return -1 if target element not found.
+//Problem 2: Gievn an sorted array of 'n' elements and a target element 't', find the index of 't' in the array. Return -1 if target element not found.
 // arr = [-5, 2, 4, 6, 10] t= 10 -> should return 2.
 
 const binarySearch = (arr, t) => {
@@ -43,3 +43,30 @@ console.log(binarySearch([-5, 2, 4, 6, 10], 4));  // Output: 2   // Big-O = O(lo
 console.log(binarySearch([-5, 2, 4, 6, 10], 6));  // Output: 3   // Big-O = O(logn)
 console.log(binarySearch([-5, 2, 4, 6, 10], 2));  // Output: 1   // Big-O = O(logn)
 console.log(binarySearch([-5, 2, 4, 6, 10], 1));  // Output: -1  // Big-O = O(logn)
+
+
+//Problem 3: Gievn an sorted array of 'n' elements and a target element 't', find the index of 't' in the array using recursive binary search. Return -1 if target element not found.
+// arr = [-5, 2, 4, 6, 10] t= 10 -> should return 2.
+
+const recursiveBinarySearch = (arr, t) => {
+    return search(arr, t, 0, arr.length - 1 );
+}
+const search = (arr, t, leftIndex, rightIndex) => {
+    if(leftIndex > rightIndex ){
+        return -1;
+    }
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+        if (arr[middleIndex] === t) {
+            return middleIndex;
+        } else if (arr[middleIndex] < t) {
+           return search(arr, t, middleIndex + 1, rightIndex);
+        } else {
+           return search(arr, t, leftIndex , middleIndex -1);
+        }
+    
+}
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10)); // Output: 4    // Big-O = O(logn)
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 4));  // Output: 2    // Big-O = O(logn)
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6));  // Output: 3    // Big-O = O(logn)
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 2));  // Output: 1    // Big-O = O(logn)
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 1));  // Output: -1   // Big-O = O(logn)
