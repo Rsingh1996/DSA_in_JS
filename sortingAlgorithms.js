ff706d4c5bb3f5ae6b525d91f6bfdd0bcfb29369
Rahul Singh <81622488+Rsingh1996@users.noreply.github.com>
2025-03-13 23:22:26 +0530

    Bubble Sort

--- sortingAlgorithms.js
+++ sortingAlgorithms.js
@@ -1,1 +1,22 @@
+// Bubble Sort
+// Problem - Given an array of integers, sort the array.
+// const arr = [-6, 20, 8, -2, 4] Should return -> [-6, -2, 4, 8, 20]
 
+const bubbleSort = (arr) => {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return arr;
+}
+
+const arr = [-6, 20, 8, -2, 4];
+console.log(bubbleSort(arr)); // Output: [-6, -2, 4, 8, 20] Big-O = O(n^2)
