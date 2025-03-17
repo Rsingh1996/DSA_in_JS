022a8c38560d9448910ce52566052d83ff5c8c6f
Rahul Singh <81622488+Rsingh1996@users.noreply.github.com>
2025-03-17 23:26:59 +0530

    Insertion Sort

--- sortingAlgorithms.js
+++ sortingAlgorithms.js
@@ -20,3 +20,24 @@ const bubbleSort = (arr) => {
 
 const arr = [-6, 20, 8, -2, 4];
 console.log(bubbleSort(arr)); // Output: [-6, -2, 4, 8, 20] Big-O = O(n^2)
+
+// Insertion Sort
+// Problem - Given an array of integers, sort the array.
+// const arr = [-6, 20, 8, -2, 4] Should return -> [-6, -2, 4, 8, 20]
+// [-6, 20, 20, 8, 4
+const insertionSort = (arr) => {
+
+        for (let i = 1; i < arr.length; i++) {
+            let numberToInsert = arr[i];
+            let j = i -1; 
+            while(j >= 0 && arr[j] > numberToInsert ){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = numberToInsert;
+            }
+    return arr;
+}
+
+const arr = [-6, 20, 8, -2, 4];
+console.log(insertionSort(arr)); // Output: [-6, -2, 4, 8, 20] Big-O = O(n^2)
