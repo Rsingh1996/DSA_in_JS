a545cea00ce750341572a16198fa9af9cf11d1ba
Rahul Singh <81622488+Rsingh1996@users.noreply.github.com>
2025-03-21 23:52:26 +0530

    Update miscProblems.js

--- miscProblems.js
+++ miscProblems.js
@@ -13,4 +13,4 @@ const cartesianProduct = (arr1, arr2) => {
 }
 const A = [1,2];
 const B = [3,4,5];
-console.log(cartesianProduct(A,B)); // Output: [ [ 1, 3 ], [ 1, 4 ], [ 1, 5 ], [ 2, 3 ], [ 2, 4 ], [ 2, 5 ] ]
+console.log(cartesianProduct(A,B)); // Output: [ [ 1, 3 ], [ 1, 4 ], [ 1, 5 ], [ 2, 3 ], [ 2, 4 ], [ 2, 5 ] ] Big-O = (n*m)
