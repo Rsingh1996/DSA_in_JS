46ee2859a82482875211c480f3df3124c01bf834
Rahul Singh <81622488+Rsingh1996@users.noreply.github.com>
2025-03-26 22:22:15 +0530

    Update miscProblems.js

--- miscProblems.js
+++ miscProblems.js
@@ -31,3 +31,15 @@ const climbingStaircase = (n) => {
     
 }
 console.log(climbingStaircase(4)); // Output: 5 Big-O = O(n)
+
+const towerOfHanoi = (n, fromRod, toRod, usingRod) => {
+    if(n === 1){
+        console.log(`Move disk 1 from ${fromRod} to ${toRod}`);
+        return
+    }
+    towerOfHanoi(n-1, fromRod, usingRod, toRod );
+    console.log(`Move disk ${n} from ${fromRod} to ${toRod}`)
+    
+    towerOfHanoi(n-1,usingRod, toRod, fromRod,  );
+}
+console.log(towerOfHanoi(3, 'A', 'C', 'B')); // Output: 5 Big-O = O(2^n)
