d9262a837ae6857f4f9e2612fd01d33913b3bf82
Rahul Singh <81622488+Rsingh1996@users.noreply.github.com>
2025-03-30 14:44:16 +0530

    Update Built-in-Function.js

--- array/Built-in-Function.js
+++ array/Built-in-Function.js
@@ -1,39 +1,39 @@
 const arr = [1, 2, 3, 'rahul'];
-arr.push('Singh');
-arr.unshift('Bangalore');
-arr.push(5)
+arr.push('Singh'); // Big-O = O(1)
+arr.unshift('Bangalore'); // Big-O = O(n)
+arr.push(5) // Big-O = O(1)
 
 for(let item of arr){
   console.log(item);
 }
 console.log('----------------')
-arr.pop()
-arr.shift()
+arr.pop() // Big-O = O(1)
+arr.shift() // Big-O = O(n)
 for(let item of arr){
   console.log(item);
 }
 const newArr = [1,2,3,4,5]
 const newMappedArr =  newArr.map(item => item*2);
-console.log(newMappedArr);
+console.log(newMappedArr); // Big-O = O(n)
 
 const newFilterredArr = newArr.filter(item => item%2);
-console.log(newFilterredArr);
+console.log(newFilterredArr); // Big-O = O(n)
 
 const newReducedArr = newArr.reduce((acc, current) => acc + current);
-console.log(newReducedArr);
+console.log(newReducedArr); // Big-O = O(n)
 
 const findResult = newArr.find(item => item > 2);
-console.log(findResult);
+console.log(findResult); // Big-O = O(n)
 
 const slicedResult = newArr.slice(0,3);
-console.log(slicedResult);
+console.log(slicedResult); // Big-O = O(n)
 
 const splicedResult = newArr.splice(0,3,10,20,30);
-console.log(splicedResult);
+console.log(splicedResult); // Big-O = O(n)
 console.log(newArr);
 
 const ifExist = newArr.includes(50);
-console.log(ifExist);
+console.log(ifExist); // Big-O = O(n)
 
 const reverse = newArr.reverse();
-console.log(reverse);
+console.log(reverse); // Big-O = O(n)
