d35e6e3ddc8734584b93355719fb1f6cb7ab106e
Rahul Singh <81622488+Rsingh1996@users.noreply.github.com>
2025-04-02 23:02:54 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,12 +1,27 @@
 const map = new Map([['a', 1], ['b', 2]]);
 map.set('c', 3);
-console.log(map.has('b'));
+console.log(map.has('b')); // output - true
 for(const [key, value] of map){
   console.log(`${key} : ${value}`);
-}
-
+} /* output -
+a : 1
+b : 2
+c : 3
+*/
+console.log(map.size); // output - 3
 map.delete('b');
 console.log('After Delete operation')
 for(const [key, value] of map){
   console.log(`${key} : ${value}`);
+} /* output -
+a : 1
+c : 3
+*/
+map.clear();
+console.log('After Clear')
+for(const [key, value] of map){
+  console.log(`${key} : ${value}`);
 }
+ /* output - Nothing is there no show after clear 
+
+*/
