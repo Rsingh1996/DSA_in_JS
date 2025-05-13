a5b2f5365cce15dd58eaa9d406d483dfcfa2c115
Rahul <rahulna1996@gmail.com>
2025-05-13 20:18:42 +0530

    implemented deletion of a node in binary search tree

--- tree/binarySearchTree.js
+++ tree/binarySearchTree.js
@@ -105,6 +105,33 @@ class BinarySearchTree {
       return this.max(root.right);
     }
   }
+
+  delete(value) {
+    this.root = this.deleteNode(this.root, value);
+  }
+
+  deleteNode(root, value) {
+    if (root === null) {
+      return root;
+    }
+    if (value < root.value) {
+      root.left = this.deleteNode(root.left, value);
+    } else if (value > root.value) {
+      root.right = this.deleteNode(root.right, value);
+    } else {
+      if (!root.left && !root.right) {
+        return null;
+      }
+      if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      }
+      root.value = this.min(root.right);
+      root.right = this.deleteNode(root.right, root.value);
+    }
+    return root;
+  }
 }
 
 const bst = new BinarySearchTree();
@@ -113,7 +140,7 @@ bst.insert(10);
 bst.insert(5);
 bst.insert(15);
 bst.insert(3);
-bst.insert(7);
+// bst.insert(7);
 console.log(`Is this tree empty ? `, bst.isEmpty());
 console.log(bst.search(bst.root, 10));
 console.log(bst.search(bst.root, 3));
@@ -130,3 +157,6 @@ console.log(`Min of tree`);
 console.log(bst.min(bst.root));
 console.log(`Max of tree`);
 console.log(bst.max(bst.root));
+console.log(`deletion from tree`);
+bst.delete(10);
+bst.levelOrder();
